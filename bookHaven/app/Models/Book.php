116f00d0cdbac19f2f73099e831bf7f19c16585c
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-11-30 20:17:38 +0100

    Add assignable fields

--- bookHaven/app/Models/Book.php
+++ bookHaven/app/Models/Book.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    // Massive assignable fields
+    protected $fillable = [
+        'title',
+        'author',
+        'genre',
+        'total_pages',
+        'status',
+        'summary',
+        'added_by_user_id',
+        'currently_reading_user_id',
+    ];
 }
