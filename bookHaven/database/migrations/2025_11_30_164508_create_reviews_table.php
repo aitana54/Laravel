8ca999e7c62952c50d7c6c7939a672d6da099342
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-11-30 17:50:14 +0100

    Add content of the review

--- bookHaven/database/migrations/2025_11_30_164508_create_reviews_table.php
+++ bookHaven/database/migrations/2025_11_30_164508_create_reviews_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            // Content of the review
+            $table->text('content');
+            $table->unsignedTinyInteger('rating');
             $table->timestamps();
         });
     }
