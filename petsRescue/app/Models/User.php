7da9bed60936322b036db356b152184cd4bc69bf
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-11-30 18:40:27 +0100

    Add on differents use to correct form

--- petsRescue/app/Models/User.php
+++ petsRescue/app/Models/User.php
@@ -10,7 +10,8 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
