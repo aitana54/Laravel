2efe6820f9afbbf120661a88a3462177300e7174
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-12-01 12:23:53 +0100

    Add space

--- bookHaven/database/migrations/2025_12_01_105250_create_book_category_table.php
+++ bookHaven/database/migrations/2025_12_01_105250_create_book_category_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreignId('category_id')
                 ->constrained('categories')
                 ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
