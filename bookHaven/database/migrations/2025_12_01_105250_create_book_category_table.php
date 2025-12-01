3d4111ebb2423ff0fecee8bcb5903b0f1df01922
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-12-01 12:13:21 +0100

    Remove id pivot table

--- bookHaven/database/migrations/2025_12_01_105250_create_book_category_table.php
+++ bookHaven/database/migrations/2025_12_01_105250_create_book_category_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('book_category', function (Blueprint $table) {
-            $table->id();
+
             $table->timestamps();
         });
     }
