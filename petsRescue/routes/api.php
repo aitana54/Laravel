3fc344d95b33d7c08fa6afafbd6de5c2108bcef1
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-12-02 16:40:35 +0100

    Add route get

--- petsRescue/routes/api.php
+++ petsRescue/routes/api.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PetController;
 
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
