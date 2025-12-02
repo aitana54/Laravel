796445e2e54850d2d1cdd3c65b0840b7beb79ad4
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-12-02 16:37:13 +0100

    Remove old route

--- petsRescue/routes/api.php
+++ petsRescue/routes/api.php
@@ -5,14 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PetController;
 
 
-// Ruta /api/health
-Route::get('/health');
-
-// Ruta /api/info
-Route::get('/info', function () {
-    return response()->json([
-        'store_name' => 'BookHaven',
-        'books_in_stock' => 125,
-        'genres' => ['ficción', 'ensayo', 'infantil', 'misterio'],
-    ]);
-});
