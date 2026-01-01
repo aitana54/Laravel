74ea81c7f2eb24db6aa186ac36bfe2bf26526410
aitana54 <aitana.buiza@gracia.lasalle.cat>
2026-01-01 23:39:19 +0100

    Add category routes

--- bookHaven/routes/api.php
+++ bookHaven/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ReviewController;
 use App\Models\Review;
 use Illuminate\Http\Request;
@@ -44,3 +45,15 @@ Route::apiResource('books', BookController::class);
 // Rutas para reseñas
 Route::post('books/{book}/reviews', [ReviewController::class, 'store']); //Crear reseña
 Route::delete('books/{book}/reviews/{review}', [ReviewController::class, 'destroy']); // Eliminar reseña
+
+
+// Rutas para categorias
+Route::get('categories', [CategoryController::class, 'index']); // Listar categorías
+Route::post('categories', [CategoryController::class, 'store']); // Crear categoría
+Route::delete('categories/{category}', [CategoryController::class, 'destroy']); // Eliminar categoría
+
+// Asignar una categoría a un libro
+Route::post('books/{book}/categories', [CategoryController::class, 'assignCategoryToBook']);
+
+// Listar libros de una categoría específica
+Route::get('categories/{category}/books', [CategoryController::class, 'booksInCategory']);
