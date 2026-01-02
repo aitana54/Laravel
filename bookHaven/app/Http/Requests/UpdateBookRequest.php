6909e2ac85419f1bfb4b820019ccc4be20075fd8
aitana54 <aitana.buiza@gracia.lasalle.cat>
2026-01-02 17:01:15 +0100

    Add rules fields

--- bookHaven/app/Http/Requests/UpdateBookRequest.php
+++ bookHaven/app/Http/Requests/UpdateBookRequest.php
@@ -21,8 +21,17 @@ class UpdateBookRequest extends FormRequest
      */
     public function rules(): array
     {
+        // "sometimes" significa: Valida este campo solo si el campo existe en la petición."
         return [
-            //
+            'title' => ['sometimes', 'required', 'string', 'max:120'],
+            'author' => ['sometimes', 'required', 'string', 'max:120'],
+            'genre' => ['sometimes', 'required', 'string', 'max:100'],
+            'total_pages' => ['sometimes', 'required', 'integer', 'min:49', 'max:100000'],
+            'status' => ['sometimes', 'required', 'in:available,reading,finished'],
+            'summary' => ['sometimes', 'nullable', 'integer', 'min:150', 'max:200'],
+
+            'add_by_user_id' => ['sometimes', 'nullable', 'integer', 'exists:users,id'],
+            'currently_reading_user_id' => ['sometimes', 'nullable', 'integer', 'exists:users,id'],
         ];
     }
 }
