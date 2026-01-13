49a2a1edd2d7ed342a17beff2011629519b44ebf
aitana54 <aitana.buiza@gracia.lasalle.cat>
2026-01-13 15:48:01 +0100

    Add register and login

--- petsRescue/app/Http/Controllers/AuthController.php
+++ petsRescue/app/Http/Controllers/AuthController.php
@@ -6,5 +6,51 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        $token = $user->createToken('api')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!user || !Hash::check($data['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Credenciales inválidas.',
+            ], 401);
+        }
+        $token = $user->createToken('api')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function me(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(null, 204);
+    }
 }
