ffa30295c3f30429bcb22a7ff19ecf8e73c8fae2
aitana54 <aitana.buiza@gracia.lasalle.cat>
2026-01-13 15:57:42 +0100

    Add rules and messages

--- petsRescue/app/Http/Requests/LoginRequest.php
+++ petsRescue/app/Http/Requests/LoginRequest.php
@@ -11,7 +11,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,17 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'El email es obligatorio.',
+            'email.email' => 'El email no tiene un formato válido.',
+            'password.required' => 'La contraseña es obligatoria.',
         ];
     }
 }
