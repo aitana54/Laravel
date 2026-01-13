cfb64395e396c5261477c88857e0d4bc373d82c4
aitana54 <aitana.buiza@gracia.lasalle.cat>
2026-01-13 15:54:35 +0100

    Add Sanctum instalation

--- petsRescue/vendor/composer/installed.php
+++ petsRescue/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '049f2b05e5d4feb480bfd1a3c018377d7077ee3d',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '049f2b05e5d4feb480bfd1a3c018377d7077ee3d',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
