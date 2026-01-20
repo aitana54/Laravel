a74bea47444321f677bfd8db71c52d43f42ffd81
aitana54 <aitana.buiza@gracia.lasalle.cat>
2026-01-20 15:24:07 +0100

    Add rules register

--- bookHaven/app/Http/Requests/RegisterRequest.php
+++ bookHaven/app/Http/Requests/RegisterRequest.php
@@ -22,7 +22,9 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:120'],
+            'email' => ['required', 'email', 'max:190', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
         ];
     }
 }
