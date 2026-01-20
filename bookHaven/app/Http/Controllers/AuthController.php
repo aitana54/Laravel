6b956b5b8fa471c6e75873dbb71c39edce7e254a
aitana54 <aitana.buiza@gracia.lasalle.cat>
2026-01-20 15:05:35 +0100

    Add uses

--- bookHaven/app/Http/Controllers/AuthController.php
+++ bookHaven/app/Http/Controllers/AuthController.php
@@ -1,8 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
